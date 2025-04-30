fcdc560945f7dba75543ae38a9eeaeefc35fa83b
Sourav-Kumar-Panda <souravkumarpanda54@gmail.com>
2025-04-30 06:57:28 +0000

    Adding the files

---
+++ src/BASICS/DEMO.java
@@ -0,0 +1,11 @@
+package BASICS;
+
+import java.util.Scanner;
+
+public class DEMO {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+    }
+}
---
+++ src/BASICS/Primitives.java
@@ -0,0 +1,17 @@
+package BASICS;
+
+import java.util.Scanner;
+
+public class Primitives {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello!! "+name);
+
+        //String is not primitive
+//        String name = "sourav";
+
+        //The datatype which we cannot break further
+        // into any other datatype is called primitive data type.
+    }
+}
